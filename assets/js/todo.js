8d2ee73d0bb0af2059a4a62fecd4df3339d13d17
myc0l <miketjnr@gmail.com>
2018-05-02 13:52:29 +0200

    initial commit for todo app

---
+++ assets/js/todo.js
@@ -0,0 +1,19 @@
+$("ul").on("click", "li",function(){
+	$(this).toggleClass("completed");
+	
+});
+
+$("ul").on("click", "span", function(e){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove;
+	})
+	e.stopPropagation;
+});
+
+$("input[type ='text']").keypress(function(e){
+	if(e.which === 13){
+		var toDoText = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span> X </span>" + toDoText + "</li>");
+	}
+});
